6e6b869e4e25216aa658b0a733f612b5a263795b
ramlanrinos <e21336@eng.pdn.ac.lk>
2025-07-17 00:54:44 +0530

    Init commit

---
+++ src/main/java/com/rinos/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.rinos.controllers;
+
+import com.rinos.models.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/users")
+public class UserController {
+
+//    @GetMapping
+//    public String getUsers() {
+//        return "Hello API";
+//    }
+
+    @GetMapping
+    public List<User>  getUsers() {
+        return Arrays.asList(new User(1L, "Rinos", "rinos@gmail.com"),
+                new User(2L, "Rosy", "rosy@gmail.com"),
+                new User(3L, "Ramlan", "ramlan@gmail.com"));
+    }
+}
