e585aa73526a5d655b82a5feadfa39342e31b809
ramlanrinos <e21336@eng.pdn.ac.lk>
2025-07-17 19:37:32 +0530

    Get Single User

--- src/main/java/com/rinos/controllers/UserController.java
+++ src/main/java/com/rinos/controllers/UserController.java
@@ -4,12 +4,11 @@ import com.rinos.entity.UserEntity;
 import com.rinos.models.User;
 import com.rinos.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "api/users")
@@ -28,5 +27,15 @@ public class UserController {
 //                new User(2L, "Rosy", "rosy@gmail.com"),
 //                new User(3L, "Ramlan", "ramlan@gmail.com"));
         return userRepository.findAll();
+    }
+
+    @PostMapping
+    public UserEntity saveUser(@RequestBody UserEntity userEntity) {
+        return userRepository.save(userEntity);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<UserEntity> getUser(@PathVariable Long id) {
+        return userRepository.findById(id);
     }
 }
